3802805067997887f888e3dfbe22725fb4439138
hardeniyakapil-cmyk <hardeniya.kapil@gmail.com>
2026-06-26 10:09:38 +0530

    set

---
+++ modules.py
@@ -0,0 +1,5 @@
+#modules;predefined code written in python for direct use case
+#without managing hsrd code.
+#types of modules
+#1.in-built module(random,os,math,string,datetime)
+#2.user-defined module(custom modules)
